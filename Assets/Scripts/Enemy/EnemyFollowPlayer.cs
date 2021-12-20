8266dc708a4e54a2c6a90ab2f145135a80429ad2
edwarantoni2000 <edwarantoni2000@gmail.com>
2021-12-20 11:36:12 +0700

    update enemy

--- Assets/Scripts/Enemy/EnemyFollowPlayer.cs
+++ Assets/Scripts/Enemy/EnemyFollowPlayer.cs
@@ -6,22 +6,45 @@ public class EnemyFollowPlayer : MonoBehaviour
 {
     public float speed;
     public float lineOfSite;
+    public float rayDist;
+    private bool movingRight;
+    public Transform groundDetect;
     private Transform player;
+    Vector3 StartPosition;
     // Start is called before the first frame update
     void Start()
     {
+        StartPosition = this.transform.position;
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position, Vector2.down, rayDist);
+
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
         if (distanceFromPlayer < lineOfSite)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.position,speed*Time.deltaTime);
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (groundCheck.collider==false)
+        {
+            transform.position = new Vector3(transform.position.x, StartPosition.y,transform.position.z);
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
         }
         
+
     }
 
 
@@ -29,5 +52,6 @@ public class EnemyFollowPlayer : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, rayDist);
     }
 }
